e4dd26df3b0997085320e715cd62de1c4aeb194c
Cathella <ncathie214@gmail.com>
2022-02-02 04:32:22 +0300

    Add a markdown parser

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import './App.css';
 function App() {
   const [ markdown, setMarkdown ] = useState('# suppppp');
 
+  function handleChange(e) {
+    setMarkdown(e.target.value);
+  }
+
   return (
     <div className="app">
-      <textarea onChange={(e) => setMarkdown(e.target.value)} value={markdown} />
+      <textarea onChange={handleChange} value={markdown} />
       <div className="preview">{markdown}</div>
     </div>
   );
