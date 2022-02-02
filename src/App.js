039097e994db486151a748ec03f371d95d6a252c
Cathella <ncathie214@gmail.com>
2022-02-02 04:23:40 +0300

    Add useState to listen to typing event

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ markdown, setMarkdown ] = useState('# suppppp');
+
   return (
     <div className="app">
-      <textarea />
-      <div className="preview">
-        Render markdown here
-      </div>
+      <textarea onChange={(e) => setMarkdown(e.target.value)} value={markdown} />
+      <div className="preview">{markdown}</div>
     </div>
   );
 }
